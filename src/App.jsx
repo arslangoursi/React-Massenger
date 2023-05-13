58272d76c6cb07fe6806828e811cd12e856b9d6d
Arslan <73060834+muhammadarslan478@users.noreply.github.com>
2023-05-13 19:42:37 +0500

    ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,46 @@
 import React from "react";
-import { useState } from "react";
-// import "./index.css";
+import { Edit, Trash } from "react-feather";
+import "./index.css";
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
-  const [value, setValues] = useState("");
-  function handleSubmit() {
-    setTodos([...todos, value]);
-    setValues("");
-    console.log(value);
-  }
   return (
-    <>
-      <div className="main__container">
-        <div className="center__container">
-          <input
-            type="text"
-            onChange={(e) => {
-              setValues(e.target.value);
-            }}
-          />
-          <button onClick={handleSubmit}>Add task</button>
-          {todos.map((todo) => {
-            return <li>{todo}</li>;
-          })}
+    <div className="main__container">
+      <div className="center__container">
+        <div className="input__container__heading">Get things done!</div>
+        <div className="input__container">
+          <input type="text" placeholder="Write task here..." />
+          <button className="task__btn">Add task</button>
+        </div>
+        <div className="content__container">
+          <div className="task__entry">
+            <div className="task__entry__text">Task one here</div>
+            <div className="task__entry__edit">
+              <Edit />
+            </div>
+            <div className="task__entry__delete">
+              <Trash />
+            </div>
+          </div>
+          <div className="task__entry">
+            <div className="task__entry__text">Task one here</div>
+            <div className="task__entry__edit">
+              <Edit />
+            </div>
+            <div className="task__entry__delete">
+              <Trash />
+            </div>
+          </div>
+          <div className="task__entry">
+            <div className="task__entry__text">Task one here</div>
+            <div className="task__entry__edit">
+              <Edit />
+            </div>
+            <div className="task__entry__delete">
+              <Trash />
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
